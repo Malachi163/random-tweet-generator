7d97082f7025b95123c3ca42b7a0a453f602f591
Malachi163 <Samuellee240@gmail.com>
2019-09-09 13:11:49 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import random
+f = open(" realdonaldtrump.txt", "r")
+
+
+#big objective - make tweet generator
+# smaller objective - make a dictionary of words -> list if objects that follow
+tweets = {}
+wordBefore = ""
+
+for line in f:
+  for word in line.split():
+    if wordBefore not in tweets:
+      tweets[wordBefore] = [word]
+
+    else:
+      tweets[wordBefore].append(word)
+    
+   # print(word + " follows " + wordBefore)
+    wordBefore = word
+    
+
+    
+word = "America"
+newTweet = ""
+newTweet = newTweet + word + " "
+for i in range(140):
+  if word not in tweets:
+    break
+  nextWords = tweets[word]
+  word = (random.choice(tweets[word]))
+  newTweet = newTweet + word + " "
+  
+  
+print(newTweet)
+
+
+# word = "America"
+# newTweet = ""
+# newTweet = newTweet + word + " "
+# for i in range(140):
+#   if(word.startswith("https")):
+#     del(tweets[i])
+
+#   else:
+#     nextWords = tweets[word]
+#     word = (random.choice(tweets[word]))
+#     newTweet = newTweet + word + " "
+  
+  
+# print(newTweet)
+  
+  
+   
+
+
+   
+   
+
+    
